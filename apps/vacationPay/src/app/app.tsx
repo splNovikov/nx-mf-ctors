82fd852f0ac1589a4f7446bc7caf3e7b333494c9
pnovikov <novikovpa@yandex.ru>
2025-07-15 11:37:14 +0300

    adds ui-kit library

--- apps/vacationPay/src/app/app.tsx
+++ apps/vacationPay/src/app/app.tsx
@@ -1,10 +1,13 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
 
+import { UiKit } from '@nx-mf-ctors/ui-kit';
+
 export function App() {
   return (
     <div>
-      Vacation Pay Calculator
+      <h1>Vacation Pay Calculator</h1>
+      <UiKit />
     </div>
   );
 }
---
+++ libs/ui-kit/src/lib/ui-kit.tsx
@@ -0,0 +1,11 @@
+import styles from './ui-kit.module.scss';
+
+export function UiKit() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to UiKit!</h1>
+    </div>
+  );
+}
+
+export default UiKit;
